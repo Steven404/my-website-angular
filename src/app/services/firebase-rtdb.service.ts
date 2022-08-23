70e02030e639c651d2b8a10272935a97ea3206ba
Stefanos Michelakis <stefanosmichelakis@gmail.com>
2022-08-23 21:58:34 +0300

    Code clean up (make function on firebase service

--- src/app/services/firebase-rtdb.service.ts
+++ src/app/services/firebase-rtdb.service.ts
@@ -1,11 +1,13 @@
 import { Injectable } from '@angular/core';
 import { AngularFireDatabase } from '@angular/fire/compat/database';
 import { HotToastService } from '@ngneat/hot-toast';
-import { first } from 'rxjs';
+import { first, OperatorFunction } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
+
+
 export class FirebaseRtdbService {
 
   firstTime: boolean = true;
@@ -15,15 +17,19 @@ export class FirebaseRtdbService {
     private toast: HotToastService
   ) { }
 
+  showToast(){
+    return this.toast.observe({
+      loading: 'Retrieving data...',
+      success: 'Data retrieved!',
+      error: 'Error: Could not load data. Please download my resume from the home page or contact me at stefanosmichelakis@gmail.com for more information.',
+    })
+  }
+
   getObjectFromRTDB(url: string){
     if (this.firstTime){
       this.firstTime = false;
       return this.db.object(url).valueChanges().pipe(
-        this.toast.observe({
-          loading: 'Retrieving data...',
-          success: 'Data retrieved!',
-          error: 'Error: Could not load data. Please download my resume from the home page or contact me at stefanosmichelakis@gmail.com for more information.',
-        })
+        this.showToast()
       );
     } else {
       return this.db.object(url).valueChanges();
@@ -34,11 +40,7 @@ export class FirebaseRtdbService {
     if (this.firstTime){
       this.firstTime = false;
       return this.db.list(url).valueChanges().pipe(
-        this.toast.observe({
-          loading: 'Retrieving data...',
-          success: 'Data retrieved!',
-          error: 'Error: Could not load data. Please download my resume from the home page or contact me at stefanosmichelakis@gmail.com for more information.',
-        })
+        this.showToast() 
       );
     } else {
       return this.db.list(url).valueChanges();
